7dcde71d8906511025a49051f80937b1f1e21b47
Laurel Lao Ang <laurellaoang@gmail.com>
2022-07-25 22:42:02 +0800

    Initial commit

---
+++ src/main/java/io/biblio/versefinder/VersefinderApplication.java
@@ -0,0 +1,13 @@
+package io.biblio.versefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersefinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersefinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/biblio/versefinder/VersefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.biblio.versefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersefinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
