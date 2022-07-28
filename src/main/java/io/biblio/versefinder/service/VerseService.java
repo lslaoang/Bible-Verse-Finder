ae8f9eb4e263b16845e985bd096c833e45727f7a
Laurel Lao Ang <laurellaoang@gmail.com>
2022-07-28 12:12:46 +0800

    add VerseService

---
+++ src/main/java/io/biblio/versefinder/service/VerseService.java
@@ -0,0 +1,5 @@
+package io.biblio.versefinder.service;
+
+public interface VerseService {
+
+}
---
+++ src/main/java/io/biblio/versefinder/service/VerseServiceImpl.java
@@ -0,0 +1,7 @@
+package io.biblio.versefinder.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VerseServiceImpl implements VerseService{
+}
