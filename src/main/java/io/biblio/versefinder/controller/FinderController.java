fa7b04edd9d708862c0265de22a8056eb88aad84
Laurel Lao Ang <laurellaoang@gmail.com>
2022-09-12 20:07:56 +0800

    skeletal implementation

--- src/main/java/io/biblio/versefinder/controller/FinderController.java
+++ src/main/java/io/biblio/versefinder/controller/FinderController.java
@@ -1,4 +1,15 @@
 package io.biblio.versefinder.controller;
 
-public class FinderController {
+import com.biblio.finder.api.FinderApi;
+import com.biblio.finder.api.model.Verse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FinderController implements FinderApi {
+    
+    @Override
+    public ResponseEntity<Verse> getVerse() {
+        return null;
+    }
 }
---
+++ src/main/java/io/biblio/versefinder/model/Verse.java
@@ -0,0 +1,7 @@
+package io.biblio.versefinder.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Verse {
+}
